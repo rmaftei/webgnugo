c5abe61142cf5e90bce08046d97bb47c7a4b5eae
rmaftei <mafteiromeo@gmail.com>
2014-06-24 17:11:26 +0300

    GOOD FOR OPENSHIFT

--- src/main/java/org/rmaftei/webgnugo/config/AppConfig.java
+++ src/main/java/org/rmaftei/webgnugo/config/AppConfig.java
@@ -1,62 +1,22 @@
 package org.rmaftei.webgnugo.config;
 
-import org.apache.commons.io.FileUtils;
 import org.rmaftei.gnugowrapper.GnuGoMode;
 import org.rmaftei.gnugowrapper.GnuGoWrapper;
-import org.rmaftei.webgnugo.util.OSUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-
-import javax.servlet.ServletContext;
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
 
 @Configuration
 @ComponentScan(basePackages = {"org.rmaftei.webgnugo"})
 public class AppConfig {
-    @Autowired
-    private ServletContext servletContext;
 
     @Bean()
     public GnuGoWrapper.Builder gnugoBuilder() {
-        String pathToGnuGO = null;
-
-        if(OSUtil.isUnix()) {
-            pathToGnuGO = "gnugo/gnugo";
-
-//            pathToGnuGO = servletContext.getRealPath("/WEB-INF/gnugo/linux/gnugo");
-
-//            chmodExecutable(pathToGnuGO);
-        }
-        else if(OSUtil.isWindows()) {
-            pathToGnuGO = servletContext.getRealPath("/WEB-INF/gnugo/windows/gnugo.exe");
-        }
+        String pathToGnuGO = "gnugo/gnugo";
 
         GnuGoWrapper.Builder gnugoBuilder = new GnuGoWrapper.Builder(pathToGnuGO, GnuGoMode.GTP);
         System.out.println(gnugoBuilder);
 
         return gnugoBuilder;
-    }
-
-    /**
-        There is a problem(on unix machines) when you put files
-        into war archives the permissions are not kept.
-        This method is making the file 'gnugo' executable
-     */
-    private void chmodExecutable(String pathToGnuGO) {
-        try {
-            String [] command = {"chmod","+x", pathToGnuGO};
-            Runtime rt = Runtime.getRuntime();
-            Process pr = rt.exec(command);
-            pr.waitFor();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 }
--- src/main/java/org/rmaftei/webgnugo/initializer/AppInitializer.java
+++ src/main/java/org/rmaftei/webgnugo/initializer/AppInitializer.java
@@ -1,6 +1,5 @@
 package org.rmaftei.webgnugo.initializer;
 
-import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -8,7 +7,6 @@ import javax.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class AppInitializer implements WebApplicationInitializer {
