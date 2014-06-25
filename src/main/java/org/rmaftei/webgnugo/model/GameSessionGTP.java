ca2bcc9770099e27b0322588c8f21d3484872344
mafteiromeo <mafteiromeo@gmail.com>
2014-06-25 13:03:15 +0300

    Implemented finalize() method

--- src/main/java/org/rmaftei/webgnugo/model/GameSessionGTP.java
+++ src/main/java/org/rmaftei/webgnugo/model/GameSessionGTP.java
@@ -16,115 +16,125 @@ import org.springframework.stereotype.Component;
 @Component(value = "GTP")
 @Scope("session")
 public class GameSessionGTP implements GameSession {
-    private static Logger LOGGER = Logger.getLogger(GameSessionASCII.class.getName());
+	private static Logger LOGGER = Logger.getLogger(GameSessionASCII.class
+			.getName());
 
-    @Autowired
-    private GnuGoWrapper.Builder gnugoBuilder;
+	@Autowired
+	private GnuGoWrapper.Builder gnugoBuilder;
 
-    private GnuGoWrapper gnugo;
+	private GnuGoWrapper gnugo;
 
-    private final GnuGoParser gnuGoParser = GnuGOModes.GTP.createParser();
+	private final GnuGoParser gnuGoParser = GnuGOModes.GTP.createParser();
 
-    private int moveNumber = 0;
+	private int moveNumber = 0;
 
-    private int playNumber = 0;
+	private int playNumber = 0;
 
-    public Result startGame(GameOptions gameOptions) {
-        this.playNumber = gameOptions.getBoardsize().getSize() * gameOptions.getBoardsize().getSize();
+	public Result startGame(GameOptions gameOptions) {
+		this.playNumber = gameOptions.getBoardsize().getSize()
+				* gameOptions.getBoardsize().getSize();
 
-        gnugo = gnugoBuilder.boardsize(gameOptions.getBoardsize())
-                .level(gameOptions.getLevel())
-                .handicap(gameOptions.getHandicap())
-                .komi(gameOptions.getKomi())
-                .build();
+		gnugo = gnugoBuilder.boardsize(gameOptions.getBoardsize())
+				.level(gameOptions.getLevel())
+				.handicap(gameOptions.getHandicap())
+				.komi(gameOptions.getKomi()).build();
 
-        try {
-            gnugo.start();
-        } catch (GnuGOWrapperException e) {
-            e.printStackTrace();
-        }
+		try {
+			gnugo.start();
+		} catch (GnuGOWrapperException e) {
+			e.printStackTrace();
+		}
 
-        gnugo.sendCommand(Command.SHOWBOARD);
+		gnugo.sendCommand(Command.SHOWBOARD);
 
-        String res = gnugo.getOutputAsString(1000L);
-        try {
-            return Result.Game(gnuGoParser.parse(res));
-        } catch (GnuGOParseException e) {
-            return Result.Error(e.getMessage());
-        }
-    }
+		String res = gnugo.getOutputAsString(1000L);
+		try {
+			return Result.Game(gnuGoParser.parse(res));
+		} catch (GnuGOParseException e) {
+			return Result.Error(e.getMessage());
+		}
+	}
 
-    public Result play(String pos) {
-        incrementMoveNumber();
+	public Result play(String pos) {
+		incrementMoveNumber();
 
-        if (Command.FINAL_SCORE.isSame(pos)) {
-            gnugo.sendCommand(Command.FINAL_SCORE);
-        } else if (Command.QUIT.isSame(pos)) {
-            gnugo.sendCommand(Command.QUIT);
-        } else {
-            gnugo.sendCommand(Command.PLAY_BLACK, pos);
-            gnugo.sendCommand(Command.SHOWBOARD);
-        }
+		if (Command.FINAL_SCORE.isSame(pos)) {
+			gnugo.sendCommand(Command.FINAL_SCORE);
+		} else if (Command.QUIT.isSame(pos)) {
+			gnugo.sendCommand(Command.QUIT);
+		} else {
+			gnugo.sendCommand(Command.PLAY_BLACK, pos);
+			gnugo.sendCommand(Command.SHOWBOARD);
+		}
 
-        return getOutput(100L);
-    }
+		return getOutput(100L);
+	}
 
-    public Result generateMove() {
-        incrementMoveNumber();
+	public Result generateMove() {
+		incrementMoveNumber();
 
-        gnugo.sendCommand(Command.GENERATE_WHITE_MOVE);
-        gnugo.sendCommand(Command.SHOWBOARD);
-        gnugo.sendCommand(Command.LAST_MOVE);
+		gnugo.sendCommand(Command.GENERATE_WHITE_MOVE);
+		gnugo.sendCommand(Command.SHOWBOARD);
+		gnugo.sendCommand(Command.LAST_MOVE);
 
-        return getOutput(2000L);
-    }
+		return getOutput(2000L);
+	}
 
-    private Result getOutput(long timeout) {
-        try {
-            String res = gnugo.getOutputAsString(addMoreTime(timeout));
+	private Result getOutput(long timeout) {
+		try {
+			String res = gnugo.getOutputAsString(addMoreTime(timeout));
 
-            LOGGER.info(res);
+			LOGGER.info(res);
 
-            Game game = gnuGoParser.parse(res);
+			Game game = gnuGoParser.parse(res);
 
-            LOGGER.info(game.toString());
-            return Result.Game(game);
-        } catch (GnuGOParseException e) {
-            return Result.Error(e.getMessage());
-        }
-    }
+			LOGGER.info(game.toString());
+			return Result.Game(game);
+		} catch (GnuGOParseException e) {
+			return Result.Error(e.getMessage());
+		}
+	}
 
-    private long addMoreTime(long timeout) {
-        if ((getMoveNumber() / playNumber) >= 0.24) {
-            timeout += 500L;
-        }
-        if ((getMoveNumber() / playNumber) >= 0.50) {
-            timeout += 1000L;
-        }
-        if ((getMoveNumber() / playNumber) >= 0.74) {
-            timeout += 1500L;
-        }
+	private long addMoreTime(long timeout) {
+		if ((getMoveNumber() / playNumber) >= 0.24) {
+			timeout += 500L;
+		}
+		if ((getMoveNumber() / playNumber) >= 0.50) {
+			timeout += 1000L;
+		}
+		if ((getMoveNumber() / playNumber) >= 0.74) {
+			timeout += 1500L;
+		}
 
-        return timeout;
-    }
+		return timeout;
+	}
 
-    public void stopGame() {
-        gnugo.stop();
-    }
+	public void stopGame() {
+		gnugo.stop();
+	}
 
-    public Result undo() {
-        gnugo.sendCommand(Command.UNDO);
-        gnugo.sendCommand(Command.UNDO);
-        gnugo.sendCommand(Command.SHOWBOARD);
+	public Result undo() {
+		gnugo.sendCommand(Command.UNDO);
+		gnugo.sendCommand(Command.UNDO);
+		gnugo.sendCommand(Command.SHOWBOARD);
 
-        return getOutput(1000L);
-    }
+		return getOutput(1000L);
+	}
 
-    public void incrementMoveNumber() {
-        this.moveNumber++;
-    }
+	public void incrementMoveNumber() {
+		this.moveNumber++;
+	}
 
-    public long getMoveNumber() {
-        return moveNumber;
-    }
+	public long getMoveNumber() {
+		return moveNumber;
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		if (gnugo != null && gnugo.live()) {
+			gnugo.stop();
+		}
+
+		super.finalize();
+	}
 }
