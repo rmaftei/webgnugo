485b0ad0175f28004aa2842f545a20757d899c4b
mafteiromeo <mafteiromeo@gmail.com>
2014-07-01 11:56:59 +0300

    security issue

--- src/main/java/org/rmaftei/webgnugo/config/SecurityConfig.java
+++ src/main/java/org/rmaftei/webgnugo/config/SecurityConfig.java
@@ -22,10 +22,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").hasRole("USER")
-				.antMatchers("/small").hasRole("USER").and().formLogin()
-				.loginPage("/login").failureUrl("/login?error").and().csrf()
-				.disable();
+		http.authorizeRequests().antMatchers("/").fullyAuthenticated()
+				.antMatchers("/small").fullyAuthenticated()
+				.antMatchers("/medium").fullyAuthenticated()
+				.antMatchers("/big").fullyAuthenticated().antMatchers("/end")
+				.fullyAuthenticated().antMatchers("/game/**")
+				.fullyAuthenticated().and().formLogin().loginPage("/login")
+				.failureUrl("/login?error").and().csrf().disable();
 
 		http.sessionManagement().maximumSessions(1)
 				.expiredUrl("/session_expired");
